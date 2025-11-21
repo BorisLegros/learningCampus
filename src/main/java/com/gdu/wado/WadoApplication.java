2d6b58e0cfd7be51cba5785fbc0fd228572aba1c
Boris LEGROS <boris.legros@gmail.com>
2025-11-21 23:39:44 +0100

    sequence1

---
+++ src/main/java/com/gdu/wado/WadoApplication.java
@@ -0,0 +1,13 @@
+package com.gdu.wado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdu/wado/WadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.wado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
