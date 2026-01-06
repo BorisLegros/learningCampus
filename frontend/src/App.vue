503c6bd0483efd19d9eed73a9386f8943010bb0e
Boris LEGROS <boris.legros@gmail.com>
2026-01-06 22:38:06 +0100

    start tab component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,39 @@
 
 <template>
-  <hello-world-component/>
+<!--  <hello-world-component/>-->
 
-  <restaurant-index/>
-  <RestaurantCreate/>
 
-  <fonction-index/>
-  <fonction-create/>
 
-  <collaborateur-index/>
-  <collaborateur-create/>
+
+
+
+
+  <tab-component
+    :tabs="headers">
+
+    <template #collab>
+      <collaborateur-index/>
+      <collaborateur-create/>
+    </template>
+
+    <template #fonction>
+      <fonction-index/>
+      <fonction-create/>
+    </template>
+
+    <template #store>
+      <restaurant-index/>
+      <RestaurantCreate/>
+    </template>
+  </tab-component>
+
+
+
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+
 import HelloWorldComponent from './components/HelloWorldComponent.vue';
 import RestaurantIndex from "@/components/restaurant/RestaurantIndex.vue";
 import RestaurantCreate from "@/components/restaurant/RestaurantCreate.vue";
@@ -20,6 +41,16 @@ import CollaborateurIndex from "@/components/collaborateur/CollaborateurIndex.vu
 import CollaborateurCreate from "@/components/collaborateur/CollaborateurCreate.vue";
 import FonctionIndex from "@/components/fonction/FonctionIndex.vue";
 import FonctionCreate from "@/components/fonction/fonctionCreate.vue";
+
+
+import TabComponent from "@/components/generic/TabComponent.vue"
+
+// STATE
+const headers = ref([
+  {title: "Collaborateur", key: "collab"},
+  {title: "Fonction", key: "fonction"},
+  {title: "Restaurant", key: "store"}
+])
 </script>
 
 <style scoped></style>
---
+++ frontend/src/components/generic/TabComponent.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div>
+    <button v-for="item in tabs" :key="item.key" @click="triggerBtnTab(item.key)">
+      {{ item.title }}
+    </button>
+  </div>
+
+  <div>
+    <slot :name="index"></slot>
+  </div>
+<!--  <slot v-for="item in tabs" :name="item.key" v-if="index === item.key"></slot>-->
+
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+
+// PROPS
+
+const props = defineProps({
+  tabs: {
+    type: Array, // [{title: String, key: String}, ...]
+    required: true
+  }
+})
+
+// STATE
+const index = ref(props.tabs[0])
+
+// METHOD
+const triggerBtnTab = (key: string) => {
+  console.log(key)
+  index.value = key
+}
+</script>
+
+<style scoped>
+
+</style>
